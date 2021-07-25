f79635ca0b81d39f5a279cb2187b320345905931
PabloGQueiroz <PabloGQueiroz>
2021-07-25 10:58:31 -0300

    acrescenta IMDB e corrige erro do background principal

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -10,7 +10,20 @@ $('.owl-carousel').owlCarousel({
             items:3
         },
         1000:{
-            items:5
+            items:8
         }
     }
 })
+
+            function links1(a) {                
+                //alert("valor de a " + a);
+                if (a == 1){
+                    window.open('https://www.youtube.com/watch?v=fdcP7ZVvfgQ', '_blank');   
+                }
+                else if (a == 2){
+                    window.open('https://www.adorocinema.com/series/serie-18054/temporada-36086/', '_blank');
+                }
+                else {
+                    window.open('https://imdb.com/', '_blank');
+                }
+            }                        
